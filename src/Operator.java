1833de2fba8ff8cf6a5075ba575f2a617fc2b380
tamtasheetal <sheetaltamta.dit09@gmail.com>
2017-05-29 00:24:26 +0530

    Add files via upload

---
+++ src/Operator.java
@@ -0,0 +1,23 @@
+
+public class Operator
+{
+public static void main(String[] args)
+	{
+			int a=0001;  
+			int b=1011; 
+			int c=1001;
+			System.out.print("Result for Logical Operator:");
+			{
+				System.out.println(a<b&&a++<c);//false && true = false
+				System.out.println(a);
+				{
+			System.out.print("Result for Bitwise Operator:");
+				{
+					System.out.println(a<c&a++>=b);//false & true = false  
+					System.out.println(a);
+			}
+	}
+			}
+	}
+}
+
